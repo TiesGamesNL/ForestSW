783db69dbac0d8d634e31208a88457cbbbf7de6b
rdcmcpe <emiel.thijsseling3@gmail.com>
2016-08-15 17:20:20 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
 $field_message = $_POST['cf_message'];
 
-$mail_to = 'dedalusonline@yahoo.com';
+$mail_to = 'emiel.thijsseling3@gmail.com';
 $subject = 'Message from a site visitor '.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
@@ -18,14 +18,14 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'contact_page.html';
+		window.location = 'apply.html';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Message failed. Please, send an email to gordon@template-help.com');
-		window.location = 'contact_page.html';
+		window.location = 'apply.html';
 	</script>
 <?php
 }
